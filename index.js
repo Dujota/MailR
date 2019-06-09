35c6d0c5ef47b8488c09eb46432a5b07a8c016d6
Denis <ddujota@internationalsafety.com>
2019-06-09 16:02:36 -0400

    configured passport to use the google strategy

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
-
-app.get('/', (req, res) => res.send({ hi: 'there' }));
+/**  Call the new Google Oauth Instance with config option and pass it to passport
+ * use(), prodivdes passport with a new strategy to handle the auth process.
+ */
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
