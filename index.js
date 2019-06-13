ac0cca1e8686a379df18bd4610262f5d1cd868fa
Denis <ddujota@internationalsafety.com>
2019-06-13 15:13:09 -0400

    updated the auth startegy with keys

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+
+// Authentication
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
+const PORT = process.env.PORT || 5000; // heroku port || local
+
+// PASSPORT MIDDLEWARE
 /**  Call the new Google Oauth Instance with config option and pass it to passport
  * use(), prodivdes passport with a new strategy to handle the auth process.
  */
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: `/auth/google/callback`,
+  })
+);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  window.console.log(`Listending on Port: ${PORT}`);
+});
