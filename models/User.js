d22b1baecbc9ff681b65d761c2cf9ad9991eac67
Denis <ddujota@internationalsafety.com>
2019-07-06 20:28:01 -0400

    added a schema

--- models/User.js
+++ models/User.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose');
-
+// Schema object will define the properties for each collection (what record will look like)
 const { Schema } = mongoose;
+
+// declare the schema by passing an object to the constructor that defines property: dataType
+const userSchema = new Schema({ googleId: String });
+
+// let Mongoose know we wnat to use/create a new collection defined by the schema above.
+mongoose.model('users', userSchema);
+
+module.exports = userSchema;
